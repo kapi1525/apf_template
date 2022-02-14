791f3909a741d7c9d4873c73a0f40e5c3f208b6d
kapigames <kapigamesdev@gmail.com>
2022-02-14 01:24:33 +0100

    Add some more comments

--- src/main.hpp
+++ src/main.hpp
@@ -2,8 +2,9 @@
 #include <iostream>             // for std::cout
 
 #include "appframework.hpp"     // main header with app class and some other common things
-// #include "apf/term.hpp"      // terminal functions
-// #include "apf/logs.hpp"      // logging
+// Uncoment these if you want some extra features.
+// #include "apf/term.hpp"      // terminal functions.
+// #include "apf/logs.hpp"      // logging.
 
 
 
