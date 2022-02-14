951d11f0d3648f00ee90f5f01a77aafb3ccbddaf
kapigames <kapigamesdev@gmail.com>
2022-02-14 01:18:45 +0100

    Add some comments

--- src/main.hpp
+++ src/main.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>             // for std::cout
 
 #include "appframework.hpp"     // main header with app class and some other common things
@@ -6,11 +7,17 @@
 
 
 
+// This is main app class, you can add here what ever you want.
 class apf_template : public apf::app {
 public:
     void start();
     void run();
     void end();
 
-    version apf_template_version = {0,3,0};
+    // Thiese are inherited from base apf::app class
+    // args arguments;                  // Holds command line arguments and can parse them, look apf::args class for more info.
+    // version appframework_version;    // Holds version of appframework, i think its obvious.
+
+    // version is struct that can hold version information, it can also be printed using cout and converted to string.
+    version apf_template_version = {0,3,0};     // Version "v0.3.0"
 };
